2ad9695f08f5337acda36371a6a1060f42b76143
ehsan2793 <ehsan2793@gmail.com>
2024-08-03 10:19:38 -0700

    Initial Commit

---
+++ Validation Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Validation Practice
+//
+//  Created by Ehsan Rahimi on 8/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Validation Practice/Validation_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Validation_PracticeApp.swift
+//  Validation Practice
+//
+//  Created by Ehsan Rahimi on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Validation_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
