eabe7c1f00a12b9b3c3b2ae7a14bf7b3bb0beb64
ehsan2793 <ehsan2793@gmail.com>
2024-08-03 13:00:36 -0700

    added validation

--- Validation Practice/ContentView.swift
+++ Validation Practice/ContentView.swift
@@ -7,24 +7,52 @@
 
 import SwiftUI
 
+struct LoginFormError {
+    var email: String = ""
+    var password: String = ""
+}
+
 struct ContentView: View {
-    @State private var email = ""
-    @State private var password = ""
-    
-    var isFormValid : Bool {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var loginFormError = LoginFormError()
+
+    private func clearForm() {
+        loginFormError = LoginFormError()
+    }
+
+    var isFormValid: Bool {
+        if email.isEmpty {
+            loginFormError.email = "Email is required"
+        } else if !email.isValidEmail {
+            loginFormError.email = "Email is not correct"
+        }
+
+        if password.isEmpty {
+            loginFormError.password = "Password is required"
+        } else {
+            loginFormError.password = ""
+        }
         return !email.isEmpty && !password.isEmpty && email.isValidEmail
     }
-    
+
     var body: some View {
         Form {
             TextField("Email", text: $email)
                 .textInputAutocapitalization(.never)
+            Text(loginFormError.email)
+                .font(.caption)
+                .foregroundStyle(.red.opacity(0.6))
             SecureField("Password", text: $password)
+            Text(loginFormError.password)
+                .font(.caption)
+                .foregroundStyle(.red.opacity(0.6))
             Button("Login") {
-                
-            }.disabled(!isFormValid)
+                if isFormValid {
+                    clearForm()
+                }
+            }
         }
-
     }
 }
 
